11e7a062bd1fa1ee8d521ec2b307c5d98ce6adc1
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-01-14 23:21:49 +0100

    Setup wizard merchant trade offers

--- common/src/main/java/net/wizards/villager/WizardVillagers.java
+++ common/src/main/java/net/wizards/villager/WizardVillagers.java
@@ -10,12 +10,17 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
+import net.runes.api.RuneItems;
 import net.spell_engine.spellbinding.SpellBindingBlock;
 import net.wizards.WizardsMod;
 import net.wizards.item.Armors;
+import net.wizards.item.Weapons;
 import net.wizards.item.WizardArmor;
+
+import java.util.List;
 
 public class WizardVillagers {
     public static final String WIZARD_MERCHANT = "wizard_merchant";
@@ -41,17 +46,85 @@ public class WizardVillagers {
         );
     }
 
+    private static class Offer {
+        int level;
+        ItemStack input;
+        ItemStack output;
+        int maxUses;
+        int experience;
+        float priceMultiplier;
+
+        public Offer(int level, ItemStack input, ItemStack output, int maxUses, int experience, float priceMultiplier) {
+            this.level = level;
+            this.input = input;
+            this.output = output;
+            this.maxUses = maxUses;
+            this.experience = experience;
+            this.priceMultiplier = priceMultiplier;
+        }
+
+        public static Offer buy(int level, ItemStack item, int price, int maxUses, int experience, float priceMultiplier) {
+            return new Offer(level, item, new ItemStack(Items.EMERALD, price), maxUses, experience, priceMultiplier);
+        }
+
+        public static Offer sell(int level, ItemStack item, int price, int maxUses, int experience, float priceMultiplier) {
+            return new Offer(level, new ItemStack(Items.EMERALD, price), item, maxUses, experience, priceMultiplier);
+        }
+    }
+
     public static void register() {
         var wizardPOI = registerPOI(WIZARD_MERCHANT, SpellBindingBlock.INSTANCE);
         var wizardMerchantProfession = registerProfession(
                 WIZARD_MERCHANT,
                 RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(WizardsMod.ID, WIZARD_MERCHANT)));
-        TradeOfferHelper.registerVillagerOffers(wizardMerchantProfession, 1, factories -> {
+
+        List<Offer> wizardMerchantOffers = List.of(
+                Offer.sell(1, new ItemStack(RuneItems.get(RuneItems.RuneType.ARCANE), 8), 2, 128, 1, 0.01f),
+                Offer.sell(1, new ItemStack(RuneItems.get(RuneItems.RuneType.FIRE), 8), 2, 128, 1, 0.01f),
+                Offer.sell(1, new ItemStack(RuneItems.get(RuneItems.RuneType.FROST), 8), 2, 128, 1, 0.01f),
+                Offer.sell(2, Weapons.noviceWand.item().getDefaultStack(), 4, 12, 5, 0.2f),
+                Offer.sell(2, Weapons.arcaneWand.item().getDefaultStack(), 18, 12, 8, 0.1f),
+                Offer.sell(2, Weapons.fireWand.item().getDefaultStack(), 18, 12, 8, 0.1f),
+                Offer.sell(2, Weapons.frostWand.item().getDefaultStack(), 18, 12, 8, 0.1f),
+                Offer.buy(2, new ItemStack(Items.WHITE_WOOL, 5), 8, 12, 10, 0.05f),
+                Offer.buy(2, new ItemStack(Items.LAPIS_LAZULI, 6), 12, 12, 10, 0.05f),
+                Offer.sell(3, Armors.wizardRobeSet.head.getDefaultStack(), 15, 12, 13, 0.1f),
+                Offer.sell(3, Armors.wizardRobeSet.feet.getDefaultStack(), 15, 12, 13, 0.1f),
+                Offer.sell(4, Armors.wizardRobeSet.chest.getDefaultStack(), 20, 12, 15, 0.1f),
+                Offer.sell(4, Armors.wizardRobeSet.legs.getDefaultStack(), 20, 12, 15, 0.1f)
+            );
+
+        for(var offer: wizardMerchantOffers) {
+            TradeOfferHelper.registerVillagerOffers(wizardMerchantProfession, offer.level, factories -> {
                 factories.add(((entity, random) -> new TradeOffer(
-                        new ItemStack(Items.EMERALD, 3),
-                        Armors.wizardRobeSet.head.getDefaultStack(),
-                        6, 2, 0.02f)
+                        offer.input,
+                        offer.output,
+                        offer.maxUses, offer.experience, offer.priceMultiplier)
                 ));
+            });
+        }
+        TradeOfferHelper.registerVillagerOffers(wizardMerchantProfession, 5, factories -> {
+            factories.add(((entity, random) -> new TradeOffers.SellEnchantedToolFactory(
+                    Weapons.arcaneStaff.item(),
+                    40,
+                    3,
+                    30,
+                    0.2F).create(entity, random)
+            ));
+            factories.add(((entity, random) -> new TradeOffers.SellEnchantedToolFactory(
+                    Weapons.fireStaff.item(),
+                    40,
+                    3,
+                    30,
+                    0.2F).create(entity, random)
+            ));
+            factories.add(((entity, random) -> new TradeOffers.SellEnchantedToolFactory(
+                    Weapons.frostStaff.item(),
+                    40,
+                    3,
+                    30,
+                    0.2F).create(entity, random)
+            ));
         });
     }
 }
