de3272a63d3d6aba7dcee22a8a8b38258ee18289
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-04-08 23:49:58 +0200

    Unsmelt recipes

--- src/main/java/net/wizards/datagen/WizardsDataGenerator.java
+++ src/main/java/net/wizards/datagen/WizardsDataGenerator.java
@@ -3,9 +3,16 @@ package net.wizards.datagen;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.spell_engine.api.datagen.SimpleSoundGeneratorV2;
 import net.spell_engine.api.datagen.SpellGenerator;
+import net.spell_engine.api.item.armor.Armor;
 import net.spell_engine.rpg_series.datagen.RPGSeriesDataGen;
 import net.wizards.WizardsMod;
 import net.wizards.content.WizardSpells;
@@ -13,6 +20,7 @@ import net.wizards.content.WizardsSounds;
 import net.wizards.item.Armors;
 import net.wizards.item.Weapons;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class WizardsDataGenerator implements DataGeneratorEntrypoint {
@@ -22,6 +30,7 @@ public class WizardsDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(SoundGen::new);
         pack.addProvider(SpellGen::new);
         pack.addProvider(ItemTagGenerator::new);
+        pack.addProvider(UnsmeltGenerator::new);
     }
 
     public static class ItemTagGenerator extends RPGSeriesDataGen.ItemTagGenerator {
@@ -61,6 +70,86 @@ public class WizardsDataGenerator implements DataGeneratorEntrypoint {
                             .map(entry -> SoundEntry.withVariants(entry.id().getPath(), entry.variants()))
                             .toList()
                     )
+            );
+        }
+    }
+
+    public static class UnsmeltGenerator extends FabricRecipeProvider {
+        public UnsmeltGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, registriesFuture);
+        }
+
+        public static int UNSMELT_TIME = 300;
+
+        @Override
+        public void generate(RecipeExporter exporter) {
+            disassembleArmor(exporter, Armors.wizardRobeSet, Items.LAPIS_LAZULI);
+            disassembleArmor(exporter, Armors.arcaneRobeSet, Items.ENDER_PEARL);
+            disassembleArmor(exporter, Armors.fireRobeSet, Items.BLAZE_POWDER);
+            disassembleArmor(exporter, Armors.frostRobeSet, Items.PRISMARINE_SHARD);
+            disassembleArmor(exporter, Armors.netherite_arcane, Items.NETHERITE_SCRAP);
+            disassembleArmor(exporter, Armors.netherite_fire, Items.NETHERITE_SCRAP);
+            disassembleArmor(exporter, Armors.netherite_frost, Items.NETHERITE_SCRAP);
+
+            disassemble(exporter,
+                    List.of(Weapons.arcaneWand.item(), Weapons.fireWand.item()),
+                    Items.GOLD_NUGGET);
+            disassemble(exporter,
+                    List.of(Weapons.frostWand.item(), Weapons.fireWand.item()),
+                    Items.IRON_NUGGET);
+
+            disassemble(exporter,
+                    List.of(Weapons.arcaneStaff.item()),
+                    Items.AMETHYST_SHARD);
+            disassemble(exporter,
+                    List.of(Weapons.fireStaff.item()),
+                    Items.BLAZE_POWDER);
+            disassemble(exporter,
+                    List.of(Weapons.frostStaff.item()),
+                    Items.PRISMARINE_CRYSTALS);
+
+            disassemble(exporter,
+                    Weapons.entries.stream()
+                            .filter(entry -> entry.id().getPath().contains("netherite"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.NETHERITE_SCRAP);
+        }
+
+        private static void disassembleArmor(RecipeExporter exporter, Armor.Set armorSet, Item output) {
+            FabricRecipeProvider.offerSmelting(exporter,
+                    armorSet.pieces(),
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME,
+                    "disassemble"
+            );
+            FabricRecipeProvider.offerBlasting(exporter,
+                    armorSet.pieces(),
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME / 2,
+                    "disassemble"
+            );
+        }
+
+        private static void disassemble(RecipeExporter exporter, List<ItemConvertible> items, Item output) {
+            FabricRecipeProvider.offerSmelting(exporter,
+                    items,
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME,
+                    "disassemble"
+            );
+            FabricRecipeProvider.offerBlasting(exporter,
+                    items,
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME / 2,
+                    "disassemble"
             );
         }
     }
