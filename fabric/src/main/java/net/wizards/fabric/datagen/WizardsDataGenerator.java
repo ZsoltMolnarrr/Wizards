0e269fee8eee46029d8fa483e322dceab6b7cad5
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-17 01:31:45 +0200

    NeoForge descriptor and config

--- fabric/src/main/java/net/wizards/fabric/datagen/WizardsDataGenerator.java
+++ fabric/src/main/java/net/wizards/fabric/datagen/WizardsDataGenerator.java
@@ -1,4 +1,4 @@
-package net.wizards.datagen;
+package net.wizards.fabric.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
--- neoforge/src/main/java/net/wizards/neoforge/client/NeoForgeClientMod.java
+++ neoforge/src/main/java/net/wizards/neoforge/client/NeoForgeClientMod.java
@@ -2,8 +2,11 @@ package net.wizards.neoforge.client;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.spell_engine.client.gui.ConfigMenuScreen;
 import net.wizards.WizardsMod;
 import net.wizards.client.WizardsClientMod;
 
@@ -12,5 +15,6 @@ public class NeoForgeClientMod {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         WizardsClientMod.init();
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (modContainer, parent) -> new ConfigMenuScreen(parent));
     }
 }
