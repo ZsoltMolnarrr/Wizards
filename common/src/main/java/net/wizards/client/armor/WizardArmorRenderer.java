a48987cbfc2c99891662d80190a2b8db9e3463c6
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-08-20 21:26:04 +0200

    AzureLibArmor is back

--- common/src/main/java/net/wizards/client/armor/WizardArmorRenderer.java
+++ common/src/main/java/net/wizards/client/armor/WizardArmorRenderer.java
@@ -1,42 +1,43 @@
 package net.wizards.client.armor;
 
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRenderer;
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelibarmor.common.render.layer.AzArmorTrimLayer;
 import net.minecraft.util.Identifier;
-import net.rpg_foundation.armor_api.client.GeoArmorRenderer;
 import net.wizards.WizardsMod;
 
-public final class WizardArmorRenderer {
+public class WizardArmorRenderer extends AzArmorRenderer {
     private static final String TrimTextureT1 = "wizard_robe_generic";
     private static final String TrimTextureT2 = "spec_robe_generic";
     private static final String TrimTextureT3 = "netherite_spec_robe_generic";
-
-    private WizardArmorRenderer() { }
-
-    public static GeoArmorRenderer wizard() {
-        return make("wizard_robes", "wizard_robe", TrimTextureT1);
+    public static WizardArmorRenderer wizard() {
+        return new WizardArmorRenderer("wizard_robes", "wizard_robe", TrimTextureT1);
     }
-    public static GeoArmorRenderer arcane() {
-        return make("wizard_robes", "arcane_robe", TrimTextureT2);
+    public static WizardArmorRenderer arcane() {
+        return new WizardArmorRenderer("wizard_robes", "arcane_robe", TrimTextureT2);
     }
-    public static GeoArmorRenderer fire() {
-        return make("wizard_robes", "fire_robe", TrimTextureT2);
+    public static WizardArmorRenderer fire() {
+        return new WizardArmorRenderer("wizard_robes", "fire_robe", TrimTextureT2);
     }
-    public static GeoArmorRenderer frost() {
-        return make("wizard_robes", "frost_robe", TrimTextureT2);
+    public static WizardArmorRenderer frost() {
+        return new WizardArmorRenderer("wizard_robes", "frost_robe", TrimTextureT2);
     }
-    public static GeoArmorRenderer netheriteArcane() {
-        return make("wizard_robes", "netherite_arcane_robe", TrimTextureT3);
+    public static WizardArmorRenderer netheriteArcane() {
+        return new WizardArmorRenderer("wizard_robes", "netherite_arcane_robe", TrimTextureT3);
     }
-    public static GeoArmorRenderer netheriteFire() {
-        return make("wizard_robes", "netherite_fire_robe", TrimTextureT3);
+    public static WizardArmorRenderer netheriteFire() {
+        return new WizardArmorRenderer("wizard_robes", "netherite_fire_robe", TrimTextureT3);
     }
-    public static GeoArmorRenderer netheriteFrost() {
-        return make("wizard_robes", "netherite_frost_robe", TrimTextureT3);
+    public static WizardArmorRenderer netheriteFrost() {
+        return new WizardArmorRenderer("wizard_robes", "netherite_frost_robe", TrimTextureT3);
     }
 
-    private static GeoArmorRenderer make(String modelName, String textureName, String trimTextureName) {
-        return GeoArmorRenderer.of(
+    public WizardArmorRenderer(String modelName, String textureName, String trimTextureName) {
+        super(AzArmorRendererConfig.builder(
                 Identifier.of(WizardsMod.ID, "geo/" + modelName + ".geo.json"),
                 Identifier.of(WizardsMod.ID, "textures/armor/" + textureName + ".png"))
-                .trim(Identifier.of(WizardsMod.ID, "armor/trim/" + trimTextureName), false);
+                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(WizardsMod.ID, "armor/trim/" + trimTextureName), false))
+                .build()
+        );
     }
 }
