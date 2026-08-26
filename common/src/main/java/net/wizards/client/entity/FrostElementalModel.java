bb2c74d19c8c3d04f52c2a84b50c7032e4d83c92
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-08-27 01:05:37 +0200

    Prepare sources for mapping migration

--- common/src/main/java/net/wizards/client/entity/FrostElementalModel.java
+++ common/src/main/java/net/wizards/client/entity/FrostElementalModel.java
@@ -1,6 +1,12 @@
 package net.wizards.client.entity;
 
-import net.minecraft.client.model.*;
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.entity.animation.Animation;
 import net.minecraft.client.render.entity.animation.AnimationDefinition;
 import net.minecraft.client.render.entity.model.EntityModel;
